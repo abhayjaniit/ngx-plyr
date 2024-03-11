163c662fcc41a91538b60e0b4ebd956b89443f24
abhay jani <abhayjaniit@gmail.com>
2024-03-11 16:55:24 +0530

    update plyr

--- src/app/dashjs-plyr-driver/dashjs-plyr-driver.spec.ts
+++
@@ -1,9 +0,0 @@
-import { TestBed } from '@angular/core/testing';
-import { DashjsPlyrDriver } from './dashjs-plyr-driver';
-
-describe('DashjsPlyrDriver', () => {
-  it('should be created', () => {
-    const driver = new DashjsPlyrDriver(true);
-    expect(driver).toBeTruthy();
-  });
-});
--- src/app/hlsjs-plyr-driver/hlsjs-plyr-driver.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HlsjsPlyrDriver } from './hlsjs-plyr-driver';
-
-describe('HlsjsPlyrDriver', () => {
-  it('should be created', () => {
-    const driver = new HlsjsPlyrDriver(true);
-    expect(driver).toBeTruthy();
-  });
-});
